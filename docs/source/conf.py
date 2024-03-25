11736e09d03ad3f4579992f426429603fc9f3d4f
Rohit Goswami <rog32@hi.is>
2024-03-25 01:07:52 +0000

    DOC: Configure the sphinx-build

---
+++ docs/source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "openblas_buildsys_snips"
+copyright = "2024, Rohit Goswami"
+author = "Rohit Goswami"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "autodoc2",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
+    "sphinx_contributors",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinxcontrib.spelling",
+]
+
+autodoc2_render_plugin = "myst"
+autodoc2_packages = [
+    f"../../src/{project}",
+]
+
+myst_enable_extensions = [
+    "deflist",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
---
+++ docs/source/index.md
@@ -0,0 +1,17 @@
+# OpenBLAS `meson.build` generators
+
+A set of functions meant for parsing bits of the OpenBlas build system, both
+`Makefile` and `CMakefile` ariants.
+
+```{toctree}
+:maxdepth: 2
+:caption: Contents
+
+apidocs/index
+```
+
+## Indices and tables
+
+- [](genindex)
+- [](modindex)
+- [](search)
